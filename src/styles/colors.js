0342601a305370e0b60a414ed8e27f514eedd960
ichwn <ichwanarif26@gmail.com>
2020-02-12 20:57:12 +0700

    add styles libs

---
+++ src/styles/colors.js
@@ -0,0 +1,26 @@
+const colors = {
+  primary: {
+    blue: "#3BA2FB",
+    orange: "#F7A325",
+    tosca: "#B2DCD7",
+    white: "#FFFFFF",
+    black: "#000000",
+    gray: "#909090"
+  },
+  light: {
+    blue: "#3EB9FE",
+    orange: "#FFCA7A",
+    green: "#EEE8A6",
+    gray: "#F2F3F6",
+    black: "#424242"
+  },
+  dark: {
+    gray: "#5E6374",
+    orange: "#ED9106"
+  },
+  stroke: {
+    gray: "#8D91A1"
+  }
+}
+
+export default colors
---
+++ src/styles/index.js
@@ -0,0 +1,7 @@
+import { Platform } from 'react-native';
+ 
+export { default as Colors } from './colors';
+// export { default as Fonts } from './fonts';
+// export { default as Sizes } from './sizes';
+// export { default as Responsive } from './responsive';
+// export { default as Responsives } from './responsives';
